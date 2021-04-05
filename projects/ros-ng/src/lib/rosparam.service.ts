70c9df1cae35109c17144a25da56d23b26551e73
cridito <andy.porstmann@gmx.de>
2021-04-05 21:24:36 +0200

    update

--- projects/ros-ng/src/lib/rosparam.service.ts
+++ projects/ros-ng/src/lib/rosparam.service.ts
@@ -1,52 +1,39 @@
 import { Injectable } from '@angular/core';
 import { RosconnectService } from './rosconnect.service';
 import { Subject, Observable } from 'rxjs';
-import { catchError, map, tap } from 'rxjs/operators';
+
 
 declare const ROSLIB: any;
 
-@Injectable({
-  providedIn: 'root'
-})
+@Injectable()
 export class RosParamService {
-
-
-  //params: any[] = [];
 
   constructor(
     private pepper: RosconnectService,
-    //private logService: LogService
+    private logService: LogService
   ) {
 
   }
-  getRosParam(paramName: string): any {
-    return new ROSLIB.Param({ros : this.pepper.ros,name : paramName});
+
+  getRosParam(paramName: string): Observable<any> {
+    const param = new ROSLIB.Param({ros : this.pepper.ros,name : paramName});
+    paramSub = new Subject<any>();
+    param.get(
+      value => paramSub.next(value)
+    );
+    return paramSub.asObservable().pipe(
+      tap(message => this.logService.setLogParam(paramName, message)));
   }
 
-// getRosParam(paramName: string): {observable:Observable<any>,index: number} {
-//   const param = new ROSLIB.Param({ros : this.pepper.ros,name : paramName});
-//   this.params.push({param: param, subject: new Subject<any>()});
-//   const index = this.params.length - 1;
-//   this.params[index].param.get(
-//     value => {
-//       this.params[index].subject.next(value);
-//       //this.logService.setLogParam(paramName, value);
-//     }
-//   );
-//   return {
-//     observable: this.params[index].subject.asObservable().pipe(
-//     tap(message => this.logService.setLogParam(paramName, message))),index: index};
-// }
+  setRosParam(paramName: string): Observable<any> {
+    const param = new ROSLIB.Param({ros : this.pepper.ros,name : paramName});
+    paramSub = new Subject<any>();
+    param.set(
+      value => paramSub.next(value)
+    );
+    return paramSub.asObservable().pipe(
+      tap(message => this.logService.setLogParam(paramName, message)));
+  }
 
-// unsubscribeParamSub(index: number): void {
-// this.params[index].param.unsubscribe();
-// }
-// setRosParam(paramName: string, value:any ): void{
-//   const param = new ROSLIB.Param({ros : this.pepper.ros,name : paramName})
-//   param.set(
-//     value,
-//     result => {console.log(result);
-//     this.logService.setLogParam(paramName, value, result);}
-//   );
-// }
+
 }
