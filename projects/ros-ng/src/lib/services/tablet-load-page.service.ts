8a83ddd122f71642365b719a8a97b209f56d9124
cridito <andy.porstmann@gmx.de>
2021-04-05 21:04:02 +0200

    add tablet-load-page service

---
+++ projects/ros-ng/src/lib/services/tablet-load-page.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { Subject } from 'rxjs';
+import { RosServiceService } from '../rosservice.service';
+
+@Injectable()
+export class TabletLoadPageService {
+
+    constructor(
+        private rosService: RosServiceService
+    ) {
+
+    }
+    serviceName: string = '/tablet_webview';
+    serviceType: string = 'faps_pepper_ros_bridge/TabletWebview';
+    request: { [name : string] : string } = {name : ''};
+
+  loadPage(url: string): Subject<Object>{
+    this.request.name = url;
+    return this.rosService.callService(this.serviceName, this.serviceType, this.request);
+  }
+}
