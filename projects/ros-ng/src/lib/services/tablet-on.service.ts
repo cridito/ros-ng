a9673543004958e92ca7744e64462b15c1f2ae9f
cridito <andy.porstmann@gmx.de>
2021-04-05 21:04:22 +0200

    add tablet-on service

---
+++ projects/ros-ng/src/lib/services/tablet-on.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { Subject } from 'rxjs';
+import { RosServiceService } from '../rosservice.service';
+
+@Injectable()
+export class TabletOnService {
+
+    constructor(
+        private rosService: RosServiceService
+    ) {
+
+    }
+    serviceName: string = '/tablet_webviewblank';
+    serviceType: string = 'faps_pepper_ros_bridge/TabletWebviewBlank';
+
+  displayOn(): Subject<Object>{
+    return this.rosService.callService(this.serviceName, this.serviceType);
+  }
+}
