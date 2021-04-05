713b64eed62e3f6645c4d135f49265b4a632c3b3
cridito <andy.porstmann@gmx.de>
2021-04-05 21:01:49 +0200

    update

--- projects/ros-ng/src/public-api.ts
+++ projects/ros-ng/src/public-api.ts
@@ -5,27 +5,30 @@
 // Module
 export * from './lib/ros-ng.module';
 
+// Dict
+export * from './lib/goal-messages'
 
-// Componententen
+// Componente
 export * from './lib/ros-ng.component';
-// RosconnectService
-export * from './lib/rosconnect.service';
 
 // Ros-Services
 export * from './lib/services/animatedspeech.service';
-export * from './lib/services/language.service';
-export * from './lib/services/life.service';
+export * from './lib/services/get-language.service';
+export * from './lib/services/set-language.service';
+export * from './lib/services/get-volumne.service';
+export * from './lib/services/set-volumne.service';
 export * from './lib/services/rest.service';
-export * from './lib/services/stiffness.service';
-export * from './lib/services/tablet.service';
-export * from './lib/services/volume.service';
+export * from './lib/services/tablet-load-page.service';
+export * from './lib/services/tablet-on.service';
+export * from './lib/services/tablet-off.service';
 export * from './lib/services/wakeup.service';
 
 // Ros-Topics
-export * from './lib/topics/behaviors.service';
-export * from './lib/topics/client.service';
+export * from './lib/topics/behavior.service';
+export * from './lib/topics/behavior-status.service';
+export * from './lib/topics/client_count.service';
+export * from './lib/topics/clients.service';
 export * from './lib/topics/shore.service';
 export * from './lib/topics/speech.service';
 
 // Ros-Param
-export * from './lib/rosparam.service'
