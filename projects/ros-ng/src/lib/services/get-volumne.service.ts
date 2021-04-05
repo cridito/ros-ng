b935e7674bfb6ff7c46b32d0987086780d13d48a
cridito <andy.porstmann@gmx.de>
2021-04-05 21:03:23 +0200

    add get-vaolumne service

---
+++ projects/ros-ng/src/lib/services/get-volumne.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { Subject } from 'rxjs';
+import { RosServiceService } from '../rosservice.service';
+
+@Injectable()
+export class GetVolumneService {
+
+    constructor(
+        private rosService: RosServiceService
+    ) {
+
+    }
+    serviceName: string = '/volume_state';
+    serviceType: string = 'faps_pepper_ros_bridge/GetVolume';
+
+
+    getVolumne(): Subject<Object>  {
+        return this.rosService.callService(this.serviceName, this.serviceType);
+    }
+}
