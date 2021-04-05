6e42de39ed9b1086291aec348f2385cd7fa4b1b5
cridito <andy.porstmann@gmx.de>
2021-04-05 21:02:19 +0200

    delete

--- projects/ros-ng/src/lib/services/life.service.ts
+++
@@ -1,9 +0,0 @@
-import { Injectable } from '@angular/core';
-import { RosServiceService } from '../rosservice.service';
-@Injectable({
-  providedIn: 'root'
-})
-export class LifeService {
-
-  constructor() { }
-}
